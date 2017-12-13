9776fb411db8a77f7159466cdf2b15f3f1c92bc1
athompson1991 <alexthompson1991@hotmail.com>
2017-12-12 16:44:36 -0800

    New tests

--- tests/testthat/test-timeseries.R
+++ tests/testthat/test-timeseries.R
@@ -9,6 +9,18 @@ obs <- rnorm(300)
 test_df <- data.frame(companies, states, dates = rep(dates, each=6), obs)
 test_groupr <- get_groups(test_df, groups = c("companies", "states", "dates"), functions = list(avg = "mean(obs)"))
 
-test_that("Extract xts functions properly", {
-  extract_xts(grouping_obj = test_groupr, value_choice = "avg", date_col = "dates", groups = list("companies", "states"))
+test_that("extract_xts gives right classes", {
+  extracted_groupr <- extract_xts(grouping_obj = test_groupr, value_choice = "avg", date_col = "dates", groups = list("companies", "states"))
+  expect_identical(class(extracted_groupr), "list")
+  expect_identical(class(extracted_groupr$n_1_group), "list")
+  expect_identical(class(extracted_groupr$n_1_group$dates), c("xts", "zoo"))
+  expect_identical(class(extracted_groupr$n_2_group$companies...dates), c("xts", "zoo"))
+  expect_identical(class(extracted_groupr$n_2_group$states...dates), c("xts", "zoo"))
+})
+
+test_that("extract_xts gives right names", {
+  extracted_groupr <- extract_xts(grouping_obj = test_groupr, value_choice = "avg", date_col = "dates", groups = list("companies", "states"))
+  expect_identical(names(extracted_groupr), c("n_1_group", "n_2_group"))
+  expect_identical(names(extracted_groupr$n_1_group), "dates")
+  expect_identical(names(extracted_groupr$n_2_group), c("companies...dates", "states...dates"))
 })
