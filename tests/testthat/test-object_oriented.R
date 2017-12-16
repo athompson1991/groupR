99534bcd3b25c36e5c76baaf3a8dfe97fe37fc98
Alex Thompson <alexthompson1991@hotmail.com>
2017-12-15 22:20:00 -0800

    Added test for print function

---
+++ tests/testthat/test-object_oriented.R
@@ -0,0 +1,20 @@
+context("Object oriented")
+
+function_list <- list(avg_salary = "mean(salary)", max_salary = "max(salary)")
+
+test_groupr <- get_groups(
+  df = main_df,
+  groups = c("company", "party", "color"),
+  functions = function_list
+)
+
+test_that("print works correctly", {
+  string <- capture.output(print(test_groupr))
+  expect_equal(paste0(string, collapse="\n"), "n_1_group\n  |_company\n  |_party\n  |_color\nn_2_group\n  |_company...party\n  |_company...color\n  |_party...color\nn_3_group\n  |_company...party...color")
+  string <- capture.output(print(test_groupr, include_colnames = T))
+  expect_equal(paste0(string, collapse="\n"), paste0(
+                                              "n_1_group\n  |_company\n    |_company\n    |_avg_salary\n    |_max_salary\n  |_party\n    |_party\n    |_avg_salary\n    |_max_salary\n  |_color\n    |_color\n    |_avg_salary\n    |_max_salary\n",
+                                              "n_2_group\n  |_company...party\n    |_company\n    |_party\n    |_avg_salary\n    |_max_salary\n  |_company...color\n    |_company\n    |_color\n    |_avg_salary\n    |_max_salary\n  |_party...color\n    |_party\n    |_color\n    |_avg_salary\n    |_max_salary\n",
+                                              "n_3_group\n  |_company...party...color\n    |_company\n    |_party\n    |_color\n    |_avg_salary\n    |_max_salary", collapse="")
+               )
+})
